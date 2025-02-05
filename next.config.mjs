98c8f651862e822e5a6452c364e063d6d8db298b
Agamista0 <yousef21-01715@student.eelu.edu.eg>
2025-02-05 13:29:17 +0200

    updata all files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'query',
+              key: '_nextAlias',
+              value: 'true',
+            },
+          ],
+          destination: '/:path*',
+        },
+      ],
+    };
+  },
+  // Add trailing slash configuration to ensure consistent handling
+  trailingSlash: false,
+};
 
 export default nextConfig;
